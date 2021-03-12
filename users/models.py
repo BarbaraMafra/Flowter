3a74d542df4d4b2062621a88390d15c81f224093
Barb <49295334+Banbii@users.noreply.github.com>
2021-03-12 09:19:54 -0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,8 +1,6 @@
-  
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,5 +9,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-
-
+    def get_username(self):
+        return f'{self.user.username}'
+    
+    def get_email(self):
+        return f'{self.user.email}'
