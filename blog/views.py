f9913268229177250b5822cdbd5419b1bc731e3a
Barb <49295334+Banbii@users.noreply.github.com>
2021-03-17 11:20:19 -0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -9,18 +9,87 @@ from django.views.generic import (
 )
 from .models import Post
 from users.models import Profile
+from django.http import JsonResponse
+
+from friendship.exceptions import AlreadyExistsError
+from friendship.models import Block, Follow, Friend, FriendshipRequest
+
+# friendship
+from django.contrib.auth.models import User
+from friendship.models import Friend, Follow, Block
+
+#from minhas_tags import teste_dahora
 
 def home(request):
+    lista_amigos = [request.user]
+    for usuario in Friend.objects.friends(request.user):
+        lista_amigos.append(usuario)
     context = {
-        'posts': Post.objects.order_by("-date_posted")
+        'posts': Post.objects.filter(author__in=lista_amigos).order_by("-date_posted")
     }
     #print(a[0].get_author()) # TESTE MEUaaa
     return render(request, 'blog/home.html', context)
 
+def verifica_seguindo(request, nominho=None):
+    # pega todos os colegas
+    try:
+        friend_list = Friend.objects.friends(request.user)
+    except:
+        print('failed to get friend list')
+    
+    seguindo = False
+    for i in friend_list:
+        if i.get_username() == nominho:
+            seguindo = True
+    return seguindo
+
+# lista solicitaçõe enviadas
+def verifica_request_enviado(request, nominho=None):
+    for r in Friend.objects.sent_requests(user=request.user):
+        if str(r.to_user) == nominho:
+            return True
+    return False
+
+# pega o id do request enviado
+def get_request_enviado(request, nominho=None):
+    for r in Friend.objects.sent_requests(user=request.user):
+        if str(r.to_user) == nominho:
+            return r
+    return False
+
+# lista solicitações de amizade
+def verifica_request_recebidos(request, nominho=None):
+    for friend_request in Friend.objects.unread_requests(user=request.user):
+        if str(friend_request.from_user) == nominho:
+            return True
+    return False
+
+# pega o id do request recebio
+def get_request_recebido(request, nominho=None):
+    for r in Friend.objects.unread_requests(user=request.user):
+        if str(r.from_user) == nominho:
+            return r
+    return False
+
 def profile(request, nominho=None):
+    a = Friend.objects.unrejected_requests(user=request.user)
+    for i in a:
+        print(i.from_user_id)
     a = Post.objects.order_by("-date_posted") # todos os posts
     b = [] # onde sera armazenado os posts necessários pra pagina
     c = Profile.objects.all() # todos os perfis
+
+    # pega todos os colegas
+    # try:
+    #     friend_list = Friend.objects.friends(request.user)
+    # except:
+    #     print('failed to get friend list')
+    
+    # seguindo = False
+    # for i in friend_list:
+    #     if i.get_username() == nominho:
+    #         seguindo = True
+
 
     # pega as info do user pra colocar no profile
     for i in c:
@@ -39,25 +108,96 @@ def profile(request, nominho=None):
         'autor': autor,
         'email': email,
         'url_pic': url_pic,
-        'posts': b
+        'posts': b,
+        'seguindo': verifica_seguindo(request, nominho),
+        'solicitado': verifica_request_enviado(request, nominho),
+        'eu_mesmo': request.user.get_username() == nominho
     }
-
+    print(context)
     return render(request, 'blog/profile.html', context)
 
 def search(request):
     nominho = request.GET.get('nominho', '')
-
-    encontrados = []
+    d = {
+        'usuarios': []
+    } # dicionario que contem informação de todos os perfis achados
     perfis = Profile.objects.all()
     for perfil in perfis:
         if(nominho.lower() in perfil.get_username().lower()):
-            print(f'{nominho} is inside {perfil.get_username()}')
-            encontrados.append(perfil)
-        
-    context = {
-        'perfis': encontrados
+            usuario = {
+                'perfil': perfil,
+                'seguindo': verifica_seguindo(request, perfil.get_username()),
+                'solicitado': verifica_request_enviado(request, perfil.get_username()),
+                'eu_mesmo': request.user.get_username() == perfil.get_username()
+            }
+            d['usuarios'].append(usuario)
+    return render(request, 'blog/search.html', d)
+# api do cancel request
+def cancel_request(request):
+    nominho = request.GET.get('nominho', '')
+    id_do_request = get_request_enviado(request, nominho)
+    id_do_request.cancel()
+# api pra aceitar colega
+def accept_request(request):
+    nominho = request.GET.get('nominho', '')
+    id_do_request = get_request_recebido(request, nominho)
+    print(id_do_request)
+    print(nominho)
+    id_do_request.accept()
+# api para recusr amigo
+def reject_request(request):
+    nominho = request.GET.get('nominho', '')
+    id_do_request = get_request_recebido(request, nominho)
+    id_do_request.cancel()
+# api pra remover amigo
+def rm_amigo(request):
+    amigo_a_remover = request.GET.get('amigo_a_remover', '')
+    perfis = Profile.objects.all()
+    a_remover = None
+
+    for perfil in perfis:
+        if(amigo_a_remover.lower() in perfil.get_username().lower()):
+            a_remover = perfil.user
+
+    Friend.objects.remove_friend(request.user, a_remover)
+# api pra add amigo
+def add_amigo(request):
+    destinatario = request.GET.get('destinatario', '')
+    perfis = Profile.objects.all()
+    for p in perfis:
+        if p.get_username() == destinatario:
+            perfil = p
+    #print('id do cara:', perfil.user_id)
+    #print(request.user.id)
+    data = {
+        'username': perfil.get_username(),
+        'meuzamigo': [],
+        'sucesso': False
     }
-    return render(request, 'blog/search.html', context)
+    try:
+        Friend.objects.add_friend(
+            request.user,                    # The sender
+            perfil.user,                     # The recipient
+        )
+        data['sucesso'] = True
+    except:
+        print('failed to add FRIEND cuz yes')
+        data['sucesso'] = False
+    # try:
+    #     friend_request = FriendshipRequest.objects.get(to_user=perfil.user)
+    # except:
+    #     print('failed to get friend list')
+    # try:
+    #     friend_request.accept()
+    # except:
+    #     print('failed to accept request')
+
+    # retorna um jefferson com a lista de amigos
+    # e o usuario :thumbsup:
+    a = Friend.objects.friends(request.user)
+    for i in a:
+        data['meuzamigo'].append(i.get_username())
+    return JsonResponse(data)
 
 class PostListView(ListView):
     model = Post
